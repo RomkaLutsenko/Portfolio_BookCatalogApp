82ac19a56d9039127fe20791e8512524851f6212
RomkaLutsenko <RomanLutsenko2001@gmail.com>
2022-05-24 00:24:47 +0700

    yaNeZnau

---
+++ Firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+
+const firebaseApp = firebase.initializeApp({
+	apiKey: "AIzaSyBYjdl-oXvVetGOXl9iBeS5xqwZPrb5YOA",
+	authDomain: "bookapp-e9e2c.firebaseapp.com",
+	projectId: "bookapp-e9e2c",
+	storageBucket: "bookapp-e9e2c.appspot.com",
+	messagingSenderId: "444770714151",
+	appId: "1:444770714151:web:8d301b6e7decd00e5e8402"
+});
+
+
+const db = firebaseApp.firestore();
+
+export default db;
--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
-function App() {
-return (
-<div>
-	<h1>Привет!!!!</h1>
-</div>
-);
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import firebase from './Firebase';
+
+class App extends React.Component {
+
+	books = firebase.firestore().collection("books").orderBy('rating');
+
+	state = {
+		isLoading: true,
+		data: [],
+		setdata: [],
+	}
+
+	getData() {
+		this.books.onSnapshot((querySnapshot) => {
+			const items = []
+			querySnapshot.forEach((doc) => {
+				items.push(doc.data())
+			})
+			this.setdata(items)
+		})
+	}
+
+	useEffect(() {
+		getData()
+		console.log(data);
+	}, []) /* ???????????????????????? */
+
+componentDidMount() {
+	setTimeout(() => { this.setState({ isLoading: false }) }, 30)
+}
+
+render() {
+	const { isLoading, books } = this.state;
+	return <div>{isLoading ? "Loading..." : (books.map(book =>
+		<div>
+			<h1 className="title"> {book.title}asdasd</h1>
+			<div>
+				<span>
+					asdasdasd
+				</span>
+			</div>
+		</div>
+	))}</div>
+}
 }
 
 export default App;
